47c367e58f89397dac70b872dc95bef352ca2595
Jrge-V <jorgeverduzco91@gmail.com>
2024-07-26 14:54:30 -0700

    question 3

---
+++ C1M4/Practice Quiz Lists/Q3.py
@@ -0,0 +1,15 @@
+def pig_latin(text):
+    say = "ay"
+    # Separate the text into words
+    words = text.split()
+    pig = []
+    for word in words:
+        # Create the pig latin word and add it to the list
+        word = word[1::] + word[0] + say
+        pig.append(word)
+
+    return pig
+
+
+print(pig_latin("hello how are you"))  # Should be "ellohay owhay reaay ouyay"
+print(pig_latin("programming in python is fun"))  # Should be "rogrammingpay niay ythonpay siay unfay"
